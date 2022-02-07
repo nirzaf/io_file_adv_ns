382131856f520c045c45d570b1feb0c02c1edc5d
nirzaf <mfmfazrin1986@gmail.com>
2022-02-07 20:42:22 +0530

    Add project files.

---
+++ io_file_adv/Form1.Designer.cs
@@ -0,0 +1,196 @@
+﻿namespace io_file_adv
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.fname_textBox1 = new System.Windows.Forms.TextBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.get_file_nametextBox1 = new System.Windows.Forms.TextBox();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.ext_textBox3 = new System.Windows.Forms.TextBox();
+            this.file_nametextBox1 = new System.Windows.Forms.TextBox();
+            this.button7 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(135, 109);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Creation";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(216, 109);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Modified";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(297, 109);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Access";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // fname_textBox1
+            // 
+            this.fname_textBox1.Location = new System.Drawing.Point(132, 25);
+            this.fname_textBox1.Name = "fname_textBox1";
+            this.fname_textBox1.Size = new System.Drawing.Size(312, 20);
+            this.fname_textBox1.TabIndex = 3;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(51, 23);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "Browse";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(135, 83);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(252, 20);
+            this.textBox2.TabIndex = 5;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // get_file_nametextBox1
+            // 
+            this.get_file_nametextBox1.Location = new System.Drawing.Point(132, 183);
+            this.get_file_nametextBox1.Name = "get_file_nametextBox1";
+            this.get_file_nametextBox1.Size = new System.Drawing.Size(110, 20);
+            this.get_file_nametextBox1.TabIndex = 8;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(266, 209);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(118, 23);
+            this.button5.TabIndex = 7;
+            this.button5.Text = "Get Extesion";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(132, 209);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(110, 23);
+            this.button6.TabIndex = 6;
+            this.button6.Text = "Get full File Name";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // ext_textBox3
+            // 
+            this.ext_textBox3.Location = new System.Drawing.Point(266, 183);
+            this.ext_textBox3.Name = "ext_textBox3";
+            this.ext_textBox3.Size = new System.Drawing.Size(110, 20);
+            this.ext_textBox3.TabIndex = 9;
+            // 
+            // file_nametextBox1
+            // 
+            this.file_nametextBox1.Location = new System.Drawing.Point(132, 253);
+            this.file_nametextBox1.Name = "file_nametextBox1";
+            this.file_nametextBox1.Size = new System.Drawing.Size(110, 20);
+            this.file_nametextBox1.TabIndex = 11;
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(132, 279);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(110, 23);
+            this.button7.TabIndex = 10;
+            this.button7.Text = "Get File Name";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(496, 329);
+            this.Controls.Add(this.file_nametextBox1);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.ext_textBox3);
+            this.Controls.Add(this.get_file_nametextBox1);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.fname_textBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox fname_textBox1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.TextBox get_file_nametextBox1;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.TextBox ext_textBox3;
+        private System.Windows.Forms.TextBox file_nametextBox1;
+        private System.Windows.Forms.Button button7;
+    }
+}
+
---
+++ io_file_adv/Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace io_file_adv
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            this.openFileDialog1.ShowDialog();
+            this.fname_textBox1.Text = this.openFileDialog1.FileName;
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (System.IO.File.Exists(this.fname_textBox1.Text)==true )
+            {
+                 this.textBox2.Text= System.IO.File.GetCreationTime(this.fname_textBox1.Text).ToString();
+            }
+          
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (System.IO.File.Exists(this.fname_textBox1.Text) == true)
+            {
+               this.textBox2.Text = System.IO.File.GetLastWriteTime(this.fname_textBox1.Text).ToString();
+            }
+          
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (System.IO.File.Exists(this.fname_textBox1.Text) == true)
+            {     
+             this.textBox2.Text = System.IO.File.GetLastAccessTime(this.fname_textBox1.Text).ToString();
+            }
+    
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            if (System.IO.File.Exists(this.fname_textBox1.Text) == true)
+            {
+                this.get_file_nametextBox1.Text = System.IO.Path.GetFileName(this.fname_textBox1.Text);
+            }
+           
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if (System.IO.File.Exists(this.fname_textBox1.Text) == true)
+            {
+                this.ext_textBox3.Text = System.IO.Path.GetExtension (this.fname_textBox1.Text);
+            }
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            if (System.IO.File.Exists(this.fname_textBox1.Text) == true)
+            {
+                this.file_nametextBox1.Text = System.IO.Path.GetFileNameWithoutExtension(this.fname_textBox1.Text);
+            }
+        }
+    }
+}
